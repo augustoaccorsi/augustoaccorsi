607fc4accdf32f8b7ef31b01f4e6fbe0516aaa8c
i851169 <augusto.accorsi@sap.com>
2021-08-02 15:14:48 -0300

    fix e-mail

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -54,7 +54,7 @@ return (
                 EMAIL
               </h2>
               <a className="text-indigo-400 leading-relaxed">
-                augusto.accorsi@gmail.com
+                augusto.acorsi@gmail.com
               </a>
               <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
                 PHONE
