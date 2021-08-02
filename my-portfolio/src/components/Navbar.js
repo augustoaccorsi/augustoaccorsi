39386aff1cfc3ad1f11f6d3f3eb13f31f2ee9c73
i851169 <augusto.accorsi@sap.com>
2021-08-02 15:35:48 -0300

    update navbar

--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -18,9 +18,6 @@ export default function Navbar() {
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
-            Testimonials
-          </a>
         </nav>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
         <a href="https://www.linkedin.com/in/jos%C3%A9-augusto-accorsi-9812a188/" className="mr-5 hover:text-white">
@@ -37,7 +34,6 @@ export default function Navbar() {
           href="#contact"
           className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
           Contact Me
-          <ArrowRightIcon className="w-4 h-4 ml-1" />
         </a>
       </div>
     </header>
